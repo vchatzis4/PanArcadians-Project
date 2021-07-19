34467f5c5df110b0dd91b10531de5326bbd69ede
Vasilis Chatzis <57777275+vchatzis4@users.noreply.github.com>
2021-07-19 12:15:23 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': 'SourceSansPro'
+      }
+    },
+    screens: {
+      'sm': '640px',
+      'md': '960px',
+      'lg': '1280px'
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
